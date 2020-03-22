7b05ff7fcab18e26c70666f6bda5bf1750ccdc7c
Mithileshwar Venkataraman <mithileshwar@me.com>
2020-03-23 00:56:15 +0530

    bug fixes

--- Base Server/server.js
+++ Base Server/server.js
@@ -49,26 +49,27 @@ router.get('/', function (req, res) {
 router.route('/AddEmployee')
     .post(function (req, res) {
         const employee = {
+            id: req.body.id,
             name: req.body.name,
         }
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db("mydb");
-            dbo.collection("employee").insertOne(employee, function (err, res) {
+            dbo.collection("employees").insertOne(employee, function (err, result) {
                 if (err) throw err;
-                console.log("1 document inserted");
+                res.json({ message: '1 document inserted!' });
                 db.close();
             });
         });
     });
 router.route('/getEmployees').get(function (req, res) {
-
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("mydb");
         dbo.collection("employees").find({}).toArray(function (err, result) {
             if (err) throw err;
             console.log(result);
+            res.json(result)
             db.close();
         });
     });
@@ -79,11 +80,13 @@ router.route('/getEmployee/:employee_id')
             if (err) throw err;
             var dbo = db.db("mydb");
             var query = {
-                _id: req.params.employee_id,
+                id: +req.params.employee_id,
             }
+            console.log(query)
             dbo.collection("employees").findOne(query, function (err, result) {
                 if (err) throw err;
                 console.log(result.name);
+                res.json(result)
                 db.close();
             });
         });
@@ -93,11 +96,11 @@ router.route('/updateEmployee/:employee_id')
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db("mydb");
-            var myquery = { _id: req.params.employee_id };
-            var newvalues = { $set: req.body };
-            dbo.collection("customers").updateOne(myquery, newvalues, function (err, res) {
+            var myquery = { id: +req.params.employee_id };
+            var newvalues = { $set: { name: req.body.name } };
+            dbo.collection("employees").updateOne(myquery, newvalues, function (err, result) {
                 if (err) throw err;
-                console.log("1 document updated");
+                res.json({ message: '1 document updated!' });
                 db.close();
             });
         });
@@ -108,10 +111,10 @@ router.route('/deleteEmployee/:employee_id')
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             var dbo = db.db("mydb");
-            var myquery = { _id: req.params.employee_id };
-            dbo.collection("customers").deleteOne(myquery, function (err, obj) {
+            var myquery = { id: +req.params.employee_id };
+            dbo.collection("employees").deleteOne(myquery, function (err, obj) {
                 if (err) throw err;
-                console.log("1 document deleted");
+                res.json({ message: '1 document deleted!' });
                 db.close();
             });
         });
